62d9d0926977a81a48ec1de726a561f55d9e6c51
Klyde <moradeyo.klyde@gmail.com>
2023-04-10 18:49:32 +0100

    Add new vars to varaible.tf

--- terraform-manifests/variables.tf
+++ terraform-manifests/variables.tf
@@ -1,22 +1,80 @@
-# AWS Region
+########################
+#     General AWS      #
+########################
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
   default = "eu-west-2"
 }
 
-# AWS EC2 Instance Type
+########################
+#       AWS EC2        #
+########################
 variable "instance_type" {
   description = "EC2 Instnace Type"
   type = string
   default = "t3.micro"
 }
 
-# AWS EC2 Instance Key Pair
+variable "ami" {
+    type = string
+    default = "ami-005e54dee72cc1d00"
+}
+
 variable "instance_keypair" {
   description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
   type = string
   default = "terraform-key"
 }
+
+variable "network_interface_id" {
+  type = string
+  default = "network_id_from_aws"
+}
+
+########################
+#        VPC           #
+########################
+variable "vpc_id" {
+  description = "VPC for security group"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_id" {
+  description = "VPC subnet id to place the instance"
+  type        = string
+  default     = ""
+}
+
+########################
+#         Tags         #
+########################
+variable "name" {
+  description = "Name to use for servers, tags, etc (e.g. minecraft)"
+  type        = string
+  default     = "minecraft"
+}
+
+variable "namespace" {
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
+  type        = string
+  default     = "games"
+}
+
+variable "environment" {
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+  type        = string
+  default     = "prod"
+}
+
+variable "tags" {
+  description = "Any extra tags to assign to objects"
+  type        = map
+  default     = {}
+}
+
+
+
 
 
