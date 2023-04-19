9aa57c498935fbbf601af318dff3500429ecec23
Klyde <moradeyo.klyde@gmail.com>
2023-04-19 18:13:20 +0100

    Update vpc.tf

--- terraform-manifests/vpc.tf
+++ terraform-manifests/vpc.tf
@@ -2,11 +2,14 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = local.vpc_id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
 }
 
 locals {
   vpc_id    = length(var.vpc_id) > 0 ? var.vpc_id : data.aws_vpc.default.id
-  subnet_id = length(var.subnet_id) > 0 ? var.subnet_id : sort(data.aws_subnet_ids.default.ids)[0]
+  subnet_id = length(var.subnet_id) > 0 ? var.subnet_id : sort(data.aws_subnets.default.ids)[0]
 }
