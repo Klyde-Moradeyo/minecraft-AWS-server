53b1b0a5da795a4c137baf9419b5736f9d7f73b9
Klyde <moradeyo.klyde@gmail.com>
2023-04-19 18:13:48 +0100

    Modify outputs.tf

--- terraform-manifests/outputs.tf
+++ terraform-manifests/outputs.tf
@@ -1,3 +1,23 @@
 output ami_id {
     value = data.aws_ami.ubuntu.id
 }
+
+output "id" {
+  value = module.ec2_instance.id
+}
+
+output "public_ip" {
+  value = module.ec2_instance.public_ip
+}
+
+output "vpc_id" {
+  value = local.vpc_id
+}
+
+output "subnet_id" {
+  value = local.subnet_id
+}
+
+output "minecraft_server" {
+  value = "${module.ec2_instance.public_ip}:${var.mc_port}"
+}
