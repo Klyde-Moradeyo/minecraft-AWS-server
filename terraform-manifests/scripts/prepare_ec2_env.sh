f7a6c6ffa67c3fd7c79e9f3f733fb526cdb383eb
Klyde <moradeyo.klyde@gmail.com>
2023-04-23 10:22:21 +0100

    Add script for preparing ec2 server environment

---
+++ terraform-manifests/scripts/prepare_ec2_env.sh
@@ -0,0 +1,25 @@
+# Prepare Enviornment
+year=$(date +"%Y"); 
+month=$(date +"%m"); 
+day=$(date +"%d"); 
+hour=$(date +"%H"); 
+minute=$(date +"%M"); 
+second=$(date +"%S")
+
+echo """
+-----------------------------------------------------------------
+Date: $day/$month/$year $hour:$minute:$second
+-----------------------------------------------------------------
+"""
+
+# git clone relevant repos
+git clone
+# minecraft aws terraform and minecraft world 
+
+# call the install script
+
+# docker compose up -d
+
+
+store private key in ~/.ssh 
+
