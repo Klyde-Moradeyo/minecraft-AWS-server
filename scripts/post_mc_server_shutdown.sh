ac9283816134eb21a379cea9792f7a3b15c9ad8d
Klyde <moradeyo.klyde@gmail.com>
2023-05-01 13:24:38 +0100

    Update post_mc_server_shutdown to use calculate runtime

--- scripts/post_mc_server_shutdown.sh
+++ scripts/post_mc_server_shutdown.sh
@@ -3,51 +3,46 @@
 # Post Minecraft Server Container Close
 # Use: Post "terraform destroy" script to be performed on ec2 instance
 
-home_dir="/home/ubuntu"
-git_private_key_path="$home_dir/.ssh/id_rsa"
-minecraft_world_repo_dir="$home_dir/minecraft-tf-AWS-server/minecraft-data/minecraft-world"
-container_world_repo_dir="$home_dir/minecraft-tf-AWS-server/minecraft-data/world"
+source helper_functions.sh
 
-get_current_date() {
-  local year=$(date +"%Y")
-  local month=$(date +"%m")
-  local day=$(date +"%d")
-  local hour=$(date +"%H")
-  local minute=$(date +"%M")
-  local second=$(date +"%S")
+function run {
+  home_dir="/home/ubuntu"
+  git_private_key_path="$home_dir/.ssh/id_rsa"
+  minecraft_world_repo_dir="$home_dir/minecraft-tf-AWS-server/docker/minecraft-data/minecraft-world"
+  container_world_repo_dir="$home_dir/minecraft-tf-AWS-server/docker/minecraft-data/world"
 
-  echo """
-  -----------------------------------------------------------------
-  Date: $day/$month/$year $hour:$minute:$second
-  -----------------------------------------------------------------
-  """
+  # Stop the docker container
+  $(cd $home_dir/minecraft-tf-AWS-server && docker compose down)
+
+  ########################
+  # Save MC world in Git #
+  ########################
+  # Configure Git user
+  git config --global user.email "darkmango444@gmail.com"
+  git config --global user.name "dark-mango-bot"
+
+  # Replace world in minecraft-world git repo
+  rm -rfv $minecraft_world_repo_dir/world
+  cp -rfv $container_world_repo_dir $minecraft_world_repo_dir
+
+  # Go To minecraft-world repo
+  cd "$minecraft_world_repo_dir"
+
+  # Add and commit changes
+  git add .
+  git commit -m "Auto-commit: Update minecraft data"
+  git tag $(date +"%Y-%m-%d-%H-%M-%S")
+
+  # Push changes to the remote repository
+  GIT_SSH_COMMAND="ssh -i $git_private_key_path -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push origin
 }
-get_current_date
 
-# Stop the docker container
-$(cd $home_dir/minecraft-tf-AWS-server && docker compose down)
-
-########################
-# Save MC world in Git #
-########################
-# Configure Git user
-git config --global user.email "darkmango444@gmail.com"
-git config --global user.name "dark-mango-bot"
-
-# Replace world in minecraft-world git repo
-rm -rfv $minecraft_world_repo_dir/world
-cp -rfv $container_world_repo_dir $minecraft_world_repo_dir
-
-# Go To minecraft-world repo
-cd "$minecraft_world_repo_dir"
-
-# Add and commit changes
-git add .
-git commit -m "Auto-commit: Update minecraft world $(date +"%d"):$(date +"%m"):$(date +"%Y") $(date +"%H"):$(date +"%M"):$(date +"%S")"
-
-# Push changes to the remote repository
-GIT_SSH_COMMAND="ssh -i $git_private_key_path -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push origin
+# Call the run function
+start=$(date +%s.%N)
+run
+finish=$(date +%s.%N)
 
 get_current_date
+calculate_runtime $start $finish
 
 
