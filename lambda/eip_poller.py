823415f5f950221beecd07c406d38e6e3cb7f81b
Klyde <moradeyo.klyde@gmail.com>
2023-05-06 18:47:34 +0100

    Add ec2 private key generation to eip_poller.py

--- lambda/eip_poller.py
+++ lambda/eip_poller.py
@@ -2,7 +2,9 @@ import boto3
 import os
 import tempfile
 from git import Repo
-
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
 
 # Fetch the SSH key from the Parameter Store
 def get_git_ssh_key(param_name):
@@ -24,26 +26,67 @@ def git_clone(repo_url, dir, branch, ssh_key):
     custom_ssh_env["GIT_SSH_COMMAND"] = f"ssh -i {ssh_key} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
     repo = Repo.clone_from(repo_url, dir, branch=branch, env=custom_ssh_env)
+    return
+
+def create_private_key(file_name, directory):
+    # Generate an RSA key pair
+    # - public_exponent: The public exponent (e) is a value used in the RSA algorithm, usually set to 65537
+    # - key_size: The size of the key in bits, here set to 2048 bits
+    # - backend: The backend used for cryptographic operations, here we use the default_backend
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    # Serialize the private key in PEM format (Privacy-Enhanced Mail, a widely used format for storing and sending cryptographic keys)
+    # - encoding: The format used to encode the key, here PEM
+    # - format: The format used for the private key, here PKCS8 (Public-Key Cryptography Standards #8)
+    # - encryption_algorithm: The algorithm used to encrypt the key, here no encryption is used
+    private_key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    # Save the serialized private key to a file named "private_key.pem" with write binary mode
+    file_path = os.path.join(directory, file_name)
+    with open(file_path, "wb") as f:
+        f.write(private_key_pem)
+
+    os.chmod(file_path, 0o400)
+
+    # Return t he directory of the priv key
+    return os.path.abspath(file_path)
+
+
 
 def lambda_handler(event, context):
     # SSH Key name from system manager parameter store
     ssh_key_name = "dark-mango-bot-private-key" 
 
-    # Repo to clone info
-    repo_info = { 
+    # Repo to clone containing terraform manifests and scripts
+    repo = { 
         "url": "https://github.com/klydem11/minecraft-AWS-server.git", 
         "branch": "main",
         "ssh_key": f"{get_git_ssh_key(ssh_key_name)}"
     }
 
-    print(f"REPO_INFO: {repo_info}")
+    print(f"repo: {repo}")
     
     with tempfile.TemporaryDirectory() as temp_dir:
-        git_clone(repo_info["url"], temp_dir, repo_info["branch"], repo_info["ssh_key"])
-        os.remove(repo_info["ssh_key"]) # Remove the SSH key file
-    
+        git_clone(repo["url"], temp_dir, repo["branch"], repo["ssh_key"])
+        os.remove(repo["ssh_key"]) # Remove the SSH key file
 
+        # Directories
+        tf_private_key_folder = os.path.join(temp_dir, "terraform", "minecraft_infrastructure", "private-key")
+        tf_mc_infra_manifests = os.path.join(temp_dir, "terraform", "minecraft_infrastructure")
 
+        # Ec2 Instance private Key directory
+        private_key = create_private_key("terraform_key.pem", tf_private_key_folder)
 
+        # Change to minecraft_infrastrucutre dir
+        os.chdir(tf_mc_infra_manifests)
+        
 
 lambda_handler("event", "context")
