7f752600a96a7c38ed27f183533c1d77afad9488
Klyde <moradeyo.klyde@gmail.com>
2023-05-06 19:43:39 +0100

    Add more git functions and start copying EIP.txt in lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,7 +2,8 @@ import boto3
 import os
 import shutil
 import tempfile
-from git import Repo
+import requests
+from git import Repo, Actor
 from python_terraform import Terraform
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -56,7 +57,9 @@ def create_private_key(file_name, directory):
     # Return t he directory of the priv key
     return os.path.abspath(file_path)
 
-
+################################
+#         Git Functions        #
+################################
 def git_clone(repo_url, dir, branch, ssh_key):
     # Set the SSH key environment variable and disable host key checking
     custom_ssh_env = os.environ.copy()
@@ -69,8 +72,29 @@ def git_clone(repo_url, dir, branch, ssh_key):
     
     return repo
 
+def git_commit(repo, commit_message, author_name, author_email):
+    author = Actor(author_name, author_email)
+    repo.git.add(update=True)  # Stage all changes
+    repo.index.commit(commit_message, author=author)
+
+def git_push(repo, ssh_key, remote_name="origin", branch="main"):
+    # Set the SSH key environment variable and disable host key checking
+    custom_ssh_env = os.environ.copy()
+    custom_ssh_env["GIT_SSH_COMMAND"] = f"ssh -i {ssh_key} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+    
+    remote = repo.remote(remote_name)
+
+    try:
+        with repo.git.custom_environment(GIT_SSH_COMMAND=custom_ssh_env["GIT_SSH_COMMAND"]):
+            remote.push(branch)
+    except Exception as e:
+        raise Exception(f"Git push failed:\n{str(e)}")
+    
+################################
+#     Terraform Functions      #
+################################
 def terraform_init(tf_obj):
-    return_code, stdout, stderr = terraform_obj.init(capture_output=True)
+    return_code, stdout, stderr = tf_obj.init(capture_output=True)
 
     if return_code != 0:
         raise Exception(f"Terraform init failed:\n{stderr}")
@@ -100,37 +124,47 @@ def lambda_handler(event, context):
     # SSH Key name from system manager parameter store
     ssh_key_name = "dark-mango-bot-private-key" 
 
-    # Repo to clone containing terraform manifests and scripts
-    repo = { 
-        "url": "https://github.com/klydem11/minecraft-AWS-server.git", 
-        "branch": "main",
-        "ssh_key": f"{get_git_ssh_key(ssh_key_name)}"
-    }
+    # Repo containing terraform manifests and scripts
+    tf_manifest_repo = { 
+                        "url": "https://github.com/klydem11/minecraft-AWS-server.git", 
+                        "branch": "main",
+                        "ssh_key": f"{get_git_ssh_key(ssh_key_name)}"
+                        }
+    
+    # Repo contains tf_state files
+    # Warn: This is not best practice but due to cost and time it is better to store it here
+    # Enhancement: Tf_state files Should be stored in personal onedrive
+    miscellaneous_repo = { 
+                        "url": "https://github.com/klydem11/miscellaneous.git", 
+                        "branch": "main",
+                        "ssh_key": f"{get_git_ssh_key(ssh_key_name)}"
+                        }
 
-    print(f"repo: {repo}")
+    print(f"repo: {tf_manifest_repo}")
     
     with tempfile.TemporaryDirectory() as temp_dir:
-        git_clone(repo["url"], temp_dir, repo["branch"], repo["ssh_key"])
-        os.remove(repo["ssh_key"]) # Remove the SSH key file
+        git_clone(tf_manifest_repo["url"], temp_dir, tf_manifest_repo["branch"], tf_manifest_repo["ssh_key"])
+        # os.remove(repo["ssh_key"]) # Remove the SSH key file
 
         # Directories
         scripts_folder = os.path.join(temp_dir, "scripts")
+        eip_txt_file = os.path.join(temp_dir, "terraform", "eip", "EIP.txt")
 
         # Minecraft Infrasture Directories
         tf_private_key_folder = os.path.join(temp_dir, "terraform", "minecraft_infrastructure", "private-key")
         tf_mc_infra_manifests = os.path.join(temp_dir, "terraform", "minecraft_infrastructure")
         tf_mc_infra_scripts = os.path.join(tf_mc_infra_manifests, "scripts")
 
-        ## Copy the contents of scripts_folder to the tf_mc_infra_scripts
-        os.makedirs(tf_mc_infra_scripts, exist_ok=True) # Create the directory if not exist
+        ## Copy Files needed for tf_apply
         shutil.copytree(scripts_folder, tf_mc_infra_scripts, dirs_exist_ok=True)
-        
-        # Ec2 Instance private Key directory
-        private_key = create_private_key("terraform_key.pem", tf_private_key_folder)
+        shutil.copy2(eip_txt_file, tf_mc_infra_manifests)
+    
+        # Create Ec2 Instance private Key=
+        create_private_key("terraform_key.pem", tf_private_key_folder)
 
         # Create a Terraform object in minecraft_infrastrucutre dir 
         tf = Terraform(working_dir=tf_mc_infra_manifests)
-        terraform_init(tf)
-        terraform_apply(tf)
+        # terraform_init(tf)
+        # terraform_apply(tf)
         
 lambda_handler("event", "context")
