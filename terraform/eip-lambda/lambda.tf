994ea1709e5bc04e6b8531f8dd62d9234aec37a7
Klyde <moradeyo.klyde@gmail.com>
2023-05-08 02:37:13 +0100

    Update lambda_function_payload in terraform manifests

--- terraform/eip-lambda/lambda.tf
+++ terraform/eip-lambda/lambda.tf
@@ -51,17 +51,10 @@ resource "aws_iam_policy" "ssm_access" {
 ########################
 #    Lambda Function   #
 ########################
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "${path.module}/../../lambda/lambda_function_payload.zip"
-  output_path = "lambda_function_payload.zip"
-}
-
 resource "aws_lambda_function" "lambda_function" {
-  filename            = "lambda_function_payload.zip"
+  filename            = "${path.module}/../../lambda/lambda_function_payload.zip"
   function_name       = "mc-server-lambda"
   role                = aws_iam_role.iam_for_lambda.arn
-  source_code_hash    = data.archive_file.lambda.output_base64sha256
   handler             = "lambda_function.lambda_handler"
   runtime             = "python3.8"
 
