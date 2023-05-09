482051d440d5a3a4cfbee67b29fae88e9af19a63
Klyde <moradeyo.klyde@gmail.com>
2023-05-09 23:51:50 +0100

    Modify lambda function's name to use var

--- terraform/eip-lambda/lambda.tf
+++ terraform/eip-lambda/lambda.tf
@@ -53,7 +53,7 @@ resource "aws_iam_policy" "ssm_access" {
 ########################
 resource "aws_lambda_function" "lambda_function" {
   filename            = "${path.module}/../../lambda/lambda_function_payload.zip"
-  function_name       = "mc-server-lambda"
+  function_name       = "${var.name}-lambda-function"
   role                = aws_iam_role.iam_for_lambda.arn
   handler             = "lambda_function.lambda_handler"
   runtime             = "python3.8"
