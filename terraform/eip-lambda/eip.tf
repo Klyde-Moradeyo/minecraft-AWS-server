05785af70f429a117ea099635201c4023b699f3f
Klyde <moradeyo.klyde@gmail.com>
2023-05-13 17:41:12 +0100

    Remove network calls from eip.tf and move to network.tf

--- terraform/eip-lambda/eip.tf
+++ terraform/eip-lambda/eip.tf
@@ -23,25 +23,6 @@ locals {
 }
 
 ########################
-#    VPC and Subnets   #
-########################
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [local.vpc_id]
-  }
-}
-
-locals {
-  vpc_id    = length(var.vpc_id) > 0 ? var.vpc_id : data.aws_vpc.default.id
-  subnet_id = length(var.subnet_id) > 0 ? var.subnet_id : sort(data.aws_subnets.default.ids)[0]
-}
-
-########################
 #         EIP          #
 ########################
 resource "aws_eip" "mc_server_eip" {
