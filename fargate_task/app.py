775aabb1b9f4d1b09e2f796144555f2b9e27626e
Klyde <moradeyo.klyde@gmail.com>
2023-05-20 13:10:06 +0100

    Update fargate app to start running specified commands

--- fargate_task/app.py
+++ fargate_task/app.py
@@ -7,6 +7,9 @@ import json
 import logging
 from python_terraform import Terraform
 from git import Repo, Actor
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -28,6 +31,37 @@ def get_git_ssh_key(param_name):
         ssh_key_dir = ssh_key_file.name
         
     return ssh_key_dir
+
+def create_private_key(file_name, directory):
+    # Generate an RSA key pair
+    # - public_exponent: The public exponent (e) is a value used in the RSA algorithm, usually set to 65537
+    # - key_size: The size of the key in bits, here set to 2048 bits
+    # - backend: The backend used for cryptographic operations, here we use the default_backend
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    # Serialize the private key in PEM format (Privacy-Enhanced Mail, a widely used format for storing and sending cryptographic keys)
+    # - encoding: The format used to encode the key, here PEM
+    # - format: The format used for the private key, here PKCS8 (Public-Key Cryptography Standards #8)
+    # - encryption_algorithm: The algorithm used to encrypt the key, here no encryption is used
+    private_key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    # Save the serialized private key to a file named "private_key.pem" with write binary mode
+    file_path = os.path.join(directory, file_name)
+    with open(file_path, "wb") as f:
+        f.write(private_key_pem)
+
+    os.chmod(file_path, 0o400)
+
+    # Return t he directory of the priv key
+    return os.path.abspath(file_path)
 
 
 ################################
@@ -53,7 +87,7 @@ def git_commit(repo, commit_message, author_name, author_email):
 def git_push(repo, ssh_key, remote_name="origin", branch="main"):
     # Set the SSH key environment variable and disable host key checking
     custom_ssh_env = os.environ.copy()
-    custom_ssh_env["GIT_SSH_COMMAND"] = f"ssh -i {ssh_key} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+    custom_ssh_env["GIT_SSH_COMMAND"] = f"ssh -i -v {ssh_key} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
     
     remote = repo.remote(remote_name)
 
@@ -91,61 +125,55 @@ def terraform_destroy(tf_obj):
     return stdout
 
 ######################################################################
-#                       Lambda Handler                               #
+#                       Server Handler                               #
 ######################################################################
-def lambda_handler(event, context):
+def server_handler(command):
     # SSH Key name from system manager parameter store
     ssh_key_name = "dark-mango-bot-private-key" 
 
     # Repo containing terraform manifests and scripts
     tf_manifest_repo = { 
-                        "url": "https://github.com/Klyde-Moradeyo/minecraft-AWS-server.git", 
+                        "name": "tf_manifests",
+                        "url": "git@github.com:Klyde-Moradeyo/minecraft-AWS-server.git", 
                         "branch": "main",
-                        "ssh_key": f"{get_git_ssh_key(ssh_key_name)}"
+                        "ssh_key": f"{get_git_ssh_key(ssh_key_name)}",
                         }
     
-    # Repo contains tf_state files
+    tf_manifest_paths = {
+                        "tf_mc_infra_manifests": os.path.join(tf_manifest_repo["name"], "terraform", "minecraft_infrastructure"),
+                        "tf_private_key_folder": os.path.join(tf_manifest_repo["name"], "terraform", "minecraft_infrastructure", "private-key"),
+                        "tf_mc_infra_scripts": os.path.join(tf_manifest_repo["name"], "scripts")
+                        }
+                          
+    
+    # Repo contains private key of minceraft ec2 instnace
     # Warn: This is not best practice but due to cost and time it is better to store it here
-    # Enhancement: Tf_state files Should be stored in personal onedrive
+    #       Not to big of an issue in this case as the keys a short lived
     miscellaneous_repo = { 
-                        "url": "https://github.com/Klyde-Moradeyo/miscellaneous.git", 
+                        "name": "miscellaneous",
+                        "url": "git@github.com:Klyde-Moradeyo/miscellaneous.git", 
                         "branch": "main",
                         "ssh_key": f"{get_git_ssh_key(ssh_key_name)}"
                         }
-
-    print(f"repo: {tf_manifest_repo}")
     
-    with tempfile.TemporaryDirectory() as temp_dir:
-        git_clone(tf_manifest_repo["url"], temp_dir, tf_manifest_repo["branch"], tf_manifest_repo["ssh_key"])
-        # os.remove(repo["ssh_key"]) # Remove the SSH key file
+    miscellaneous_paths = { 
+                        "none": None,
+                        }
+    print(tf_manifest_repo["ssh_key"])
+    git_clone(tf_manifest_repo["url"], tf_manifest_repo["name"], tf_manifest_repo["branch"], tf_manifest_repo["ssh_key"])
+    git_clone(miscellaneous_repo["url"], miscellaneous_repo["name"], miscellaneous_repo["branch"], miscellaneous_repo["ssh_key"])
 
-        # Directories
-        scripts_folder = os.path.join(temp_dir, "scripts")
-        eip_txt_file = os.path.join(temp_dir, "terraform", "eip-lambda", "EIP.txt")
-
-        # Minecraft Infrasture Directories
-        tf_private_key_folder = os.path.join(temp_dir, "terraform", "minecraft_infrastructure", "private-key")
-        tf_mc_infra_manifests = os.path.join(temp_dir, "terraform", "minecraft_infrastructure")
-        tf_mc_infra_scripts = os.path.join(tf_mc_infra_manifests, "scripts")
-
-        ## Copy Files needed for tf_apply
-        shutil.copytree(scripts_folder, tf_mc_infra_scripts, dirs_exist_ok=True)
-        shutil.copy2(eip_txt_file, tf_mc_infra_manifests)
+    # Create a Terraform object in minecraft_infrastrucutre dir 
+    tf = Terraform(working_dir=tf_manifest_paths["tf_mc_infra_manifests"])
     
-        # Create Ec2 Instance private Key=
-        # create_private_key("terraform_key.pem", tf_private_key_folder)
-
-        # Create a Terraform object in minecraft_infrastrucutre dir 
-        tf = Terraform(working_dir=tf_mc_infra_manifests)
+    if command == "start":
+        create_private_key("terraform_key.pem", tf_manifest_repo["tf_private_key_folder"])
         terraform_init(tf)
-        # terraform_apply(tf)
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps("Success")
-    }
+        print("x is positive")
+    elif command == "stop":
+        terraform_init(tf)
+    else:
+        print("err")
         
-# lambda_handler("event", "context")
-
-# logger.info('This is an INFO level message.')
-#     logger.error('This is an ERROR level message.')
+if __name__ == "__main__":
+    server_handler(os.environ["BOT_COMMAND"])
