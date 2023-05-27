b4b449607d1cdd1151d24b24e33a9da162696624
Klyde <moradeyo.klyde@gmail.com>
2023-05-27 22:19:42 +0100

    Add source code hash to lambda.tf

--- terraform/infrastructure_handler/lambda.tf
+++ terraform/infrastructure_handler/lambda.tf
@@ -122,6 +122,7 @@ resource "aws_lambda_function" "lambda_function" {
   depends_on = [ aws_ecs_cluster.my_cluster ]
 
   filename            = "./lambda_function_payload.zip"
+  source_code_hash    = filebase64sha256("./lambda_function_payload.zip") # Check the zip file SHA256 to for any changes
   function_name       = "${var.name}-lambda-function"
   role                = aws_iam_role.iam_for_lambda.arn
   handler             = "lambda_function.lambda_handler"
