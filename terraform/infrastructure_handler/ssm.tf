041260e2929202ac95d077385596e24a66d7f5e8
Klyde <moradeyo.klyde@gmail.com>
2023-05-29 00:55:52 +0100

    Update ssm.tf

--- terraform/infrastructure_handler/ssm.tf
+++ terraform/infrastructure_handler/ssm.tf
@@ -1,6 +1,10 @@
 resource "aws_ssm_parameter" "mc_server_private_key" {
   name  = "/mc_server/private_key"
   type  = "SecureString"
-  value = ""
+  value = "null"
   key_id = "alias/aws/ssm" # Default key for Parameter Store
+}
+
+data "aws_ssm_parameter" "private_key" {
+  name = "/mc_server/private_key"
 }
