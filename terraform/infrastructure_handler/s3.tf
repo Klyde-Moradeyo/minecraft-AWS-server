bce1070811afc920e763425be0ed75b38622495c
Klyde <moradeyo.klyde@gmail.com>
2023-05-31 21:26:14 +0100

    Update log_bucket_acl

--- terraform/infrastructure_handler/s3.tf
+++ terraform/infrastructure_handler/s3.tf
@@ -19,10 +19,6 @@ resource "aws_s3_bucket" "mc_s3" {
 #   acl    = "private"
 
   force_destroy = var.bucket_force_destroy
-  
-  versioning {
-    enabled = false
-  }
 
   logging {
     target_bucket = aws_s3_bucket.log_bucket.id
@@ -40,6 +36,14 @@ resource "aws_s3_bucket_public_access_block" "mc_s3_public_access_block" {
   block_public_policy     = true # blocks the creation or modification of any new public bucket policies
   ignore_public_acls      = true # instructs Amazon S3 to ignore all public ACLs associated with the bucket and its objects
   restrict_public_buckets = true # restricts access to the bucket and its objects to only AWS services and authorized users
+}
+
+resource "aws_s3_bucket_versioning" "mc_s3" {
+  bucket = local.mc_bucket_name
+
+  versioning {
+    enabled = false
+  }
 }
 
 resource "aws_s3_bucket_ownership_controls" "mc_s3_ownership_control" {
@@ -62,6 +66,7 @@ resource "aws_s3_bucket" "log_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
+  depends_on = [aws_s3_bucket.log_bucket]
   bucket = aws_s3_bucket.log_bucket.id
   acl    = "log-delivery-write"
 }
