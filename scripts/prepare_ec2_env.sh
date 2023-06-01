c6826ba865287e0d64ece54dc7d1055255b20f0c
Klyde <moradeyo.klyde@gmail.com>
2023-06-01 23:48:51 +0100

    Modify prepare_ec2_env.sh

--- scripts/prepare_ec2_env.sh
+++ scripts/prepare_ec2_env.sh
@@ -35,7 +35,7 @@ function run {
 
   # Copy minecraft world
   mc_map_repo_folder="$repo_folder/docker/minecraft-data/minecraft-world"
-  aws s3 cp "$s3_bucket_path/minecraft-world.bundle" "$home_dir/minecraft-world.bundle"  || { echo "Failed to download Minecraft world from S3"; exit 1; }
+  aws s3 cp "$s3_bucket_path/minecraft-world.bundle" "$home_dir/minecraft-world.bundle" || { echo "Failed to download Minecraft world from S3"; exit 1; }
   mkdir -p "$mc_map_repo_folder"
   git clone "$home_dir/minecraft-world.bundle" "$mc_map_repo_folder"
   rm "$home_dir/minecraft-world.bundle" # Clean up after ourselves
