daf1b31ba40d2c57f4a272b1b1c7304090093fa4
Klyde <moradeyo.klyde@gmail.com>
2023-06-01 20:47:38 +0100

    Update s3 permission type in infra_handler

--- terraform/infrastructure_handler/s3.tf
+++ terraform/infrastructure_handler/s3.tf
@@ -1,3 +1,37 @@
+
+#############################
+# Give Current User Access  #
+#############################
+resource "aws_iam_policy" "s3_bucket_access" {
+  name        = "S3BucketAccess"
+  path        = "/"
+  description = "IAM policy for providing S3 bucket access"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.mc_s3.bucket}/*",
+          "arn:aws:s3:::${aws_s3_bucket.mc_s3.bucket}"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "user_s3_access" {
+  user       = local.user_name
+  policy_arn = aws_iam_policy.s3_bucket_access.arn
+}
+
 ########################
 #       S3 Bucket      #
 ########################
@@ -27,10 +61,10 @@ resource "aws_s3_bucket_public_access_block" "mc_s3_public_access_block" {
   bucket = aws_s3_bucket.mc_s3.id
 
   # S3 bucket-level Public Access Block configuration
-  block_public_acls       = true # blocks the creation or modification of any new public ACLs on the bucket
-  block_public_policy     = true # blocks the creation or modification of any new public bucket policies
-  ignore_public_acls      = true # instructs Amazon S3 to ignore all public ACLs associated with the bucket and its objects
-  restrict_public_buckets = true # restricts access to the bucket and its objects to only AWS services and authorized users
+  # block_public_acls       = true # blocks the creation or modification of any new public ACLs on the bucket
+  # block_public_policy     = true # blocks the creation or modification of any new public bucket policies
+  # ignore_public_acls      = true # instructs Amazon S3 to ignore all public ACLs associated with the bucket and its objects
+  # restrict_public_buckets = true # restricts access to the bucket and its objects to only AWS services and authorized users
 }
 
 resource "aws_s3_bucket_versioning" "mc_s3" {
@@ -41,20 +75,11 @@ resource "aws_s3_bucket_versioning" "mc_s3" {
   }
 }
 
-
 resource "aws_s3_bucket_ownership_controls" "mc_s3_ownership_control" {
   bucket = aws_s3_bucket.mc_s3.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
-}
-
-resource "aws_s3_bucket_acl" "mc_s3_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.mc_s3_ownership_control]
-
-  bucket = aws_s3_bucket.mc_s3.id
-  # acl    = "private"
-  acl    = "public-read"
 }
 
 ########################
