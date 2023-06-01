6f008e091911e69b12a0b4181e3fd02e8006fceb
Klyde <moradeyo.klyde@gmail.com>
2023-06-01 13:35:25 +0100

    Add mc_s3_bucket_uri to infra_handler

--- terraform/infrastructure_handler/outputs.tf
+++ terraform/infrastructure_handler/outputs.tf
@@ -38,6 +38,11 @@ output "mc_s3_bucket_arn" {
   value       = aws_s3_bucket.mc_s3.arn
 }
 
+output "mc_s3_bucket_uri" {
+  description = "The ARN of the S3 bucket for logs"
+  value       = "s3://${aws_s3_bucket.mc_s3.id}"
+}
+
 output "log_s3_bucket_name" {
   description = "The name of the S3 bucket for logs"
   value       = aws_s3_bucket.log_bucket.id
