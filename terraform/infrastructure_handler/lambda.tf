c1e17cd11e4bee47fcc98f583252aa0785d9140d
Klyde <moradeyo.klyde@gmail.com>
2023-07-22 18:09:33 +0100

    Upgrade lambda_function to python3.10 runtime and Remove requests from requiremnets

--- terraform/infrastructure_handler/lambda.tf
+++ terraform/infrastructure_handler/lambda.tf
@@ -137,7 +137,7 @@ resource "aws_lambda_function" "lambda_function" {
   function_name       = "${var.name}-lambda-function"
   role                = aws_iam_role.iam_for_lambda.arn
   handler             = "lambda_function.lambda_handler"
-  runtime             = "python3.8"
+  runtime             = "python3.10"
   timeout             = 900  # Set the timeout to the max (15 minutes)
 
   # layers = [ 
