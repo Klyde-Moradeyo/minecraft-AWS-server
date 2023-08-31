fda1246cdb1e5764def94adf4214fa8ef976f128
Klyde <klyde.moradeyo.dev@gmail.com>
2023-08-31 19:19:06 +0100

    fIXED

--- terraform/dev/minecraft_infrastructure/main.tf
+++ terraform/dev/minecraft_infrastructure/main.tf
@@ -27,7 +27,7 @@ locals {
 #           Modules            #
 ################################
 module "minecraft_server" {
-  source = "github.com/Klyde-Moradeyo/minecraft-AWS-server//modules/ec2_instance?ref=dev-tf-infra-service-modules"
+  source = "github.com/Klyde-Moradeyo/minecraft-AWS-server//modules/ec2_instance?ref=tf-infra-service-modules"
   
   # Terraform Cloud Config
   tf_cloud_org                      = var.tf_cloud_org
