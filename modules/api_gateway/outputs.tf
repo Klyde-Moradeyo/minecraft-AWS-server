f854221f5090b38af18fb32dc043cfae9aa554e1
Klyde <klyde.moradeyo.dev@gmail.com>
2023-09-01 12:43:58 +0100

    Add output stage_name in api_gateway

--- modules/api_gateway/outputs.tf
+++ modules/api_gateway/outputs.tf
@@ -4,3 +4,8 @@
 output "api_gateway_url" {
   value = aws_apigatewayv2_api.minecraft_http_api.api_endpoint
 }
+
+output "stage_name" {
+  description = "The name of the deployed stage"
+  value       = aws_apigatewayv2_stage.minecraft_stage.name
+}
