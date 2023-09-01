2b2b63da2f1b005f6c9c2c3c7c92b1af8d2b1e66
Klyde <klyde.moradeyo.dev@gmail.com>
2023-09-01 19:11:40 +0100

    Update terraform ssm output

--- modules/ssm/outputs.tf
+++ modules/ssm/outputs.tf
@@ -1,4 +1,4 @@
 output "ssm_parameter_name" {
   description = "The name of the SSM parameter for the Minecraft server private key"
-  value       = replace(aws_ssm_parameter.mc_server_private_key.name, "^/", "")
+  value       = substr(aws_ssm_parameter.mc_server_private_key.name, 1, -1)
 }
