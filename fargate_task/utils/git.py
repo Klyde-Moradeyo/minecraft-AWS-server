69acb8385b0a4ef9f612e869391ebc545aa8d30b
Klyde <klyde.moradeyo.dev@gmail.com>
2023-09-18 12:34:58 +0100

    Update git util to use git python

--- fargate_task/utils/git.py
+++ fargate_task/utils/git.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from git import Repo
 from .logger import setup_logging
 
 # Setting up logging
@@ -8,7 +9,7 @@ logger = setup_logging()
 class GitUtil:
     def __init__(self, ssh_key_path):
         self.ssh_key_path = ssh_key_path
-        self.git_ssh_command = f"ssh -i {ssh_key_path} -o StrictHostKeyChecking=no"
+        self.git_ssh_command = f"ssh -i {ssh_key_path} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
         os.environ['GIT_SSH_COMMAND'] = self.git_ssh_command
 
     def clone(self, repo_url, target_directory, branch='main'):
@@ -21,26 +22,27 @@ class GitUtil:
         :return: None
         """
         try:
-            subprocess.check_call(['git', 'clone', '-b', branch, repo_url, target_directory])
-            logger.info(f"Repository '{repo_url}' cloned successfully into '{target_directory}'.")
-        except subprocess.CalledProcessError as e:
-            logger.error(f"Failed to clone the repository: '{repo_url}'. Error: {e}")
-            raise
+            repo = Repo.clone_from(repo_url, target_directory, branch)
+        except Exception as e:
+            raise Exception(f"Git clone failed:\n{str(e)}")
+        
+        return repo
+    
+    # Should use git python
+    # def checkout(self, directory, branch):
+    #     """
+    #     Checkout to a specific branch in a Git directory.
 
-    def checkout(self, directory, branch):
-        """
-        Checkout to a specific branch in a Git directory.
-
-        :param directory: Directory of the git repo.
-        :param branch: The branch to checkout to.
-        :return: None
-        """
-        try:
-            subprocess.check_call(['git', '-C', directory, 'checkout', branch])
-            logger.info(f"Checked out to '{branch}' in '{directory}'.")
-        except subprocess.CalledProcessError as e:
-            logger.error(f"Failed to checkout to branch: '{branch}'. Error: {e}")
-            raise
+    #     :param directory: Directory of the git repo.
+    #     :param branch: The branch to checkout to.
+    #     :return: None
+    #     """
+    #     try:
+    #         subprocess.check_call(['git', '-C', directory, 'checkout', branch])
+    #         logger.info(f"Checked out to '{branch}' in '{directory}'.")
+    #     except subprocess.CalledProcessError as e:
+    #         logger.error(f"Failed to checkout to branch: '{branch}'. Error: {e}")
+    #         raise
         
     @staticmethod
     def get_git_branch() -> str:
