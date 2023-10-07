de848b2aa507978de27f60f2b0922e2ccbe01db8
Klyde <klyde.moradeyo.dev@gmail.com>
2023-10-07 22:46:54 +0100

    Add vars to init

---
+++ discord_bot/config/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+##### Maintenance Mode #####
+ENABLE_MAINTENANCE = False
+
+##### Minecraft Server Config DEFAULTS #####
+DEFAULT_VERSION = "1.20.1"
+DEFAULT_DIFFICULTY = "hard"
+DEFAULT_MODE = "survival"
+DEFAULT_EULA = "TRUE"
+DEFAULT_MAX_PLAYERS = "20"
+DEFAULT_MOTD = "Mango Minecraft Server"
+DEFAULT_TYPE = "VANILLA"
+DEFAULT_ENABLE_WHITELIST = "FALSE"
+DEFAULT_ENFORCE_WHITELIST = "FALSE"
+DEFAULT_ANNOUNCE_PLAYER_ACHIEVEMENTS = "true"
+DEFAULT_ENABLE_COMMAND_BLOCK = "false"
+DEFAULT_TZ = "Europe/London"
+DEFAULT_LOG_TIMESTAMP = "true"
+DEFAULT_ICON = "xxICONxx"
+
+##### DISCORD VARS #####
+DISCORD_CHANNEL_NAME = "Mango Minecraft"
+DISCORD_CHANNEL_CATEGORY_NAME = "BOT"
+
+
+##### BOT INIT MESSAGE #####
+INIT_MSG = {
+            "------------------------------------------------------------------------------------------------------------\n" +
+            "                                  🛠️ Initialization in progress... ⚙️                                      \n" +
+            "------------------------------------------------------------------------------------------------------------\n" 
+}
+
+##### DATA PATHS #####
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOT_MSG_PATH = os.path.join(BASE_DIR, "data", "bot_messages.yml")
+
+
+
