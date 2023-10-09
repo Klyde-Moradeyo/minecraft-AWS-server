bbd22d7749620d212cf125ec7f141bdb67bb81f6
Klyde <klyde.moradeyo.dev@gmail.com>
2023-10-09 23:01:58 +0100

    Update scheduler and bot

--- discord_bot/utils/scheduler.py
+++ discord_bot/utils/scheduler.py
@@ -13,11 +13,17 @@ class Scheduler:
         self.logger = setup_logging()  # Setting up logger
         self.dt_manager = DateTimeManager()
         self.tasks = {}
+        self.running_tasks = {} # keep track of running tasks by name
 
     def generate_task_id(self, task_name):
         return f"{task_name}_{uuid.uuid4()}"
 
     async def add_task(self, task_name: str, task_method, interval: int, *args):
+        # Check if a task with the same name is already running
+        if task_name in self.running_tasks:
+            self.logger.warning(f"A task with the name '{task_name}' is already running.")
+            return None
+
         # Check if the correct number of arguments are being passed
         sig = signature(task_method)
         num_params = max(0, len(sig.parameters) - 1)
@@ -28,6 +34,7 @@ class Scheduler:
         partial_coro = functools.partial(task_method, task_id, *args)
         self.tasks[task_id] = tasks.loop(seconds=interval)(partial_coro)
         await self.start_task(task_id, interval)
+        self.running_tasks[task_name] = task_id
         return task_id
 
     async def start_task(self, task_id, interval):
@@ -39,14 +46,26 @@ class Scheduler:
             raise ValueError(f"No task found with id: {task_id}")
 
     async def stop_task(self, task_id):
+        # Find the task_name by task_id (if necessary)
+        task_name = None
+        for name, id in self.running_tasks.items():
+            if id == task_id:
+                task_name = name
+                break
+
         if task_id in self.tasks:
             self.tasks[task_id].stop()
+            del self.tasks[task_id]
+
+            if task_name:
+                # Remove the task_name from running_tasks_by_name
+                del self.running_tasks[task_name]
             self.logger.info(f"Stopped Task '{task_id}'")
         else:
             raise ValueError(f"No task found with id: '{task_id}'")
 
     async def periodic_health_check(self):
-        self.logger.info(f"TEST - periodic_health_check running!")
+        self.logger.info(f"periodic_health_check running!")
         pass
 
     async def reset_command_scroll(self, task_id, command_scroll_msg: MessageManager, bot_response: BotResponse, channel, reset_time):
