bc362f16faca14d7aad584b7521bce22dc398402
Klyde <klyde.moradeyo.dev@gmail.com>
2023-10-09 23:26:31 +0100

    Improve error handling in scheduler

--- discord_bot/utils/scheduler.py
+++ discord_bot/utils/scheduler.py
@@ -1,82 +1,104 @@
 import uuid
 import discord
 import functools
+import logging
 from inspect import signature
 from discord.ext import tasks
+from utils.logger import setup_logging
 from utils.helpers import DateTimeManager
 from utils.bot_response import BotResponse
 from utils.message_manager import MessageManager
-from utils.logger import setup_logging
+
+class TaskNotFoundError(Exception):
+    """
+    Exception raised when a task is not found.
+    """
+    pass
 
 class Scheduler:
     def __init__(self):
-        self.logger = setup_logging()  # Setting up logger
+        self.logger = setup_logging()
         self.dt_manager = DateTimeManager()
         self.tasks = {}
 
-    def generate_task_id(self, task_name):
+    def _generate_task_id(self, task_name):
         return f"{task_name}_{uuid.uuid4()}"
 
-    async def add_task(self, task_name: str, task_method, interval: int, *args):
-        # Check if the correct number of arguments are being passed
-        sig = signature(task_method)
-        num_params = max(0, len(sig.parameters) - 1)
-        # +1 because task
-        assert num_params == len(args), f"Incorrect number of arguments for {task_method.__name__}. Expected {num_params}, got {len(args)}"
+    def _task_exists(self, task_id):
+        return task_id in self.tasks
 
-        task_id = self.generate_task_id(task_name)
-        partial_coro = functools.partial(task_method, task_id, *args)
-        self.tasks[task_id] = tasks.loop(seconds=interval)(partial_coro)
-        await self.start_task(task_id, interval)
-        return task_id
+    async def add_task(self, task_name: str, task_method, interval: int, *args):
+        """
+        Add a new task to the scheduler.
+        """
+        try:
+            sig = signature(task_method)
+            num_params = max(0, len(sig.parameters) - 1)
+            assert num_params == len(args), (f"Incorrect number of arguments for '{task_method.__name__}'. Expected '{num_params}', got '{len(args)}'")
+
+            task_id = self._generate_task_id(task_name)
+            partial_coro = functools.partial(task_method, task_id, *args)
+            self.tasks[task_id] = tasks.loop(seconds=interval)(partial_coro)
+            await self.start_task(task_id, interval)
+            return task_id
+        except Exception as e:
+            self.logger.exception(f"Scheduler - Exception in add_task: {e}")
 
     async def start_task(self, task_id, interval):
-        if task_id in self.tasks:
+        """
+        Start a scheduled task.
+        """
+        try:
+            if not self._task_exists(task_id):
+                raise TaskNotFoundError(f"No task found with id: {task_id}")
+
             self.tasks[task_id].change_interval(seconds=interval)
             self.tasks[task_id].start()
-            self.logger.info(f"Started Task '{task_id}' at check interval '{interval}'s")
-        else:
-            raise ValueError(f"No task found with id: {task_id}")
+            self.logger.info(f"Scheduler - '{task_id}' - Started at check interval '{interval}s'")
+        except Exception as e:
+            self.logger.exception(f"Scheduler - '{task_id}' - Exception in start_task: {e}")
 
-    async def stop_task(self, task_id):
-        if task_id in self.tasks:
+    async def stop_task(self, task_id, reason):
+        """
+        Stop a scheduled task.
+        """
+        try:
+            if not self._task_exists(task_id):
+                raise TaskNotFoundError(f"No task found with id: '{task_id}'")
+
             self.tasks[task_id].stop()
-            self.logger.info(f"Stopped Task '{task_id}'")
-        else:
-            raise ValueError(f"No task found with id: '{task_id}'")
+            del self.tasks[task_id]  # Remove task from the tasks dictionary
+            self.logger.info(f"Scheduler - '{task_id}' - Stopped Task - Reason: {reason}")
+        except Exception as e:
+            self.logger.exception(f"Scheduler - '{task_id}' - Exception in stop_task: {e}")
 
     async def periodic_health_check(self):
-        self.logger.info(f"TEST - periodic_health_check running!")
+        self.logger.info("periodic_health_check running!")
         pass
 
     async def reset_command_scroll(self, task_id, command_scroll_msg: MessageManager, bot_response: BotResponse, channel, reset_time):
-        """
-        Processes Command Scroll's message map to check if the time of a specific message ID has passed a certain interval.
-        """
-        try:
-            self.logger.info(f"Scheduler - '{task_id}' - Running Command Scroll Check for channel id: '{channel.id}'")
-            current_time_str = self.dt_manager.get_current_datetime()
-            dt_current_time = self.dt_manager.parse_datetime(current_time_str)
-            check_interval = self.dt_manager.get_time_delta(seconds=reset_time)
+            """
+            Reset command scroll after a specific interval.
+            """
+            try:
+                self.logger.info(f"Scheduler - '{task_id}' - Running Command Scroll Check for channel id: '{channel.id}'")
+                current_time_str = self.dt_manager.get_current_datetime()
+                dt_current_time = self.dt_manager.parse_datetime(current_time_str)
+                check_interval = self.dt_manager.get_time_delta(seconds=reset_time)
 
-            msg_list = command_scroll_msg.list_messages()
-            msg = msg_list[channel.id]
-            msg_time = self.dt_manager.parse_datetime(msg['datetime'])
+                msg_list = command_scroll_msg.list_messages()
+                msg = msg_list[channel.id]
+                # msg_id = msg["message_id"]
+                msg_time = self.dt_manager.parse_datetime(msg['datetime'])
 
-            if msg_time < dt_current_time - check_interval:
-                self.logger.info(f"Resetting command for message_id '{msg['message_id']}' in channel '{channel.id}'")
-                message = bot_response.get_cmd_scroll_msg()
-                await command_scroll_msg.edit_msg(channel, message)
-                await self.stop_task(task_id)
-                self.logger.info(f"Scheduler - '{task_id}' - Stopped reset_command_scroll for channel id: '{channel.id}'")
-        except Exception as e:
-            self.logger.exception(f"Scheduler - '{task_id}' - Exception in reset_command_scroll for channel id: '{channel.id}': {e}")
-            await self.stop_task(task_id)
-            self.logger.exception(f"Scheduler - '{task_id}' - Stopped reset_command_scroll for channel id: '{channel.id}': {e}")
+                if msg_time < dt_current_time - check_interval:
+                    self.logger.info(f"Scheduler - '{task_id}' - Resetting command for message_id '{msg['message_id']}' in channel '{channel.id}'")
+                    message = bot_response.get_cmd_scroll_msg()
+                    await command_scroll_msg.edit_msg(channel, message)
+                    await self.stop_task(task_id, "Task Finished")
+            except Exception as e:
+                self.logger.exception(f"Scheduler - '{task_id}' - Exception in reset_command_scroll for channel id: '{channel.id}': {e}")
+                await self.stop_task(task_id, "Task Error")
 
     async def poll_minecraft_server_online(self):
-        # Implement
-        pass
-
-# await self.scheduler.start_task("reset_command_scroll", 3600, message_id)
-# await self.scheduler.start_task("poll_minecraft_server_online", 300, ip, port)
+        pass  # Implement
