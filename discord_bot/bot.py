2d0b19a38b4c7c108f5c68d4cca1ecf9d5659778
Klyde <klyde.moradeyo.dev@gmail.com>
2023-10-25 18:21:44 +0100

    Reinstate deleting user message on send in discord bot

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -143,6 +143,7 @@ class MinecraftBot(commands.Cog):
 
         # Only process commands in the specified channel
         if message.channel.name == DISCORD_CHANNEL_NAME:
+            await message.delete()  # delete the user's message
             if message.content.upper().startswith("PING") and self.permission_manager.is_admin(message.author.id):
                 await self.command_scroll_msg.edit_msg(message.channel, "PONG")
 
